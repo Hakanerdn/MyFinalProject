6c5c061ea3a0dc4fe58ce70ade42d5cf79c91afa
Hakanerdn <hasanhakanerden@gmail.com>
2022-05-31 15:14:34 +0300

    12

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -29,8 +29,8 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSingleton<IProductService, ProductManager>();
-            services.AddSingleton<IProductDal, EfProductDal>();
+            //services.AddSingleton<IProductService, ProductManager>();
+            //services.AddSingleton<IProductDal, EfProductDal>();
             
         }
 
